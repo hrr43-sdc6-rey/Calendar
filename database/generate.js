68fb3736765c0688d05dad27e52a436eaf141c8a
Tom <tomhere@gmail.com>
2020-02-14 20:51:47 -0800

    generate.js with seed data working

--- database/generate.js
+++ database/generate.js
@@ -16,13 +16,75 @@ function batchWrite(data, cb) {
     });
 }
 
+// Created seed for the next 6 months
+const seed = () => {
+  const today = new Date();
+  let month = today.getMonth();
+  let year = today.getFullYear();
+  // month generator - several amount of months from now
+  const howManyMonthToGenerate = 6;
+  const data = [];
+  let neededYear = 0;
+  let nextMonth = 0;
+  // comes with year, month, neededYear, nextMonth;
+  for (let x = 0; x < howManyMonthToGenerate; x += 1) {
+    if (month === 11) {
+      neededYear += 1;
+      nextMonth = 0;
+    } else {
+      neededYear = year;
+      nextMonth = month + 1;
+    }
+    const daysInMonth = new Date(neededYear, nextMonth, 0).getDate();
+
+    // empty calendar 6*7
+    const cdr = [];
+    for (let i = 0; i < 6; i += 1) {
+      cdr[i] = [];
+      for (let j = 0; j < 7; j += 1) {
+        cdr[i][j] = {};
+      }
+    }
+
+    const weekdayOfFirstDayInMonth = new Date(year, month, 1).getDay();
+    let line = 0;
+    let place = weekdayOfFirstDayInMonth;
+    for (let i = 1; i <= daysInMonth; i += 1) {
+      cdr[line][place].day = i.toString();
+      cdr[line][place].morning = Math.random() > 0.3;
+      cdr[line][place].lunch = Math.random() > 0.3;
+      place += 1;
+      if (place === 7) {
+        line += 1;
+        place = 0;
+      }
+    }
+
+    data.push({
+      year,
+      month,
+      days: cdr,
+    });
+
+    // data.push(cdr)
+
+    if (month === 11) {
+      year += 1;
+      month = 0;
+    } else {
+      month += 1;
+    }
+  }
+  return data;
+};
+
 function getData(batchIndex, batchSize) {
   const data = [];
   for (let i = batchIndex * batchSize + 1; i <= batchIndex * batchSize + batchSize; i += 1) {
     // Push and define all my data below
     data.push({
       exp_id: i,
-      dates: '[{},{},{}]',
+      dates: JSON.stringify(seed()),
     });
     // Push and define all my data above
   }
@@ -31,8 +93,8 @@ function getData(batchIndex, batchSize) {
 
 function runBatches() {
   // set these two variables to control the overall data size
-  const batchAmt = 100;    // number of batches to run
-  const batchSize = 100;   // number of records per batch
+  const batchAmt = 1000;    // number of batches to run
+  const batchSize = 1000;   // number of records per batch
 
   let batchIndex = 0;
   function oneBatch() {

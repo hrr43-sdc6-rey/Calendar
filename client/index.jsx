126d39b97d3cf6bd9c8d3bf854b93af1b51c60f2
Tom <tomhere@gmail.com>
2020-02-24 13:58:26 -0800

    add changes

--- client/index.jsx
+++ client/index.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
-// const expId = parseInt(window.location.href.split('/').slice(-1)[0], 10) || 1;
-// console.log('expId in URL: ', expId);
+const expId = parseInt(window.location.href.split('/').slice(-1)[0], 10) || 1;
+console.log('expId in URL: ', expId);
 
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const id = urlParams.get('id') - 0;
+// const queryString = window.location.search;
+// const urlParams = new URLSearchParams(queryString);
+// const id = urlParams.get('id') - 0;
 
-console.log(`id = ${id}`);
+// console.log(`id = ${id}`);
 
-ReactDOM.render(<App expId={id || 1} />, document.getElementById('calendar'));
+ReactDOM.render(<App expId={expId || 1} />, document.getElementById('calendar'));
